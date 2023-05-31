e431cf003fb3644e9e33fde776a47c7f86217d00
Kishkinev Vlad <stud160314@vyatsu.ru>
2023-05-31 11:07:09 +0300

    5 commit

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,11 +1,16 @@
 #include "Weather.h"
+#include "JsonService.h"
 
 using namespace std;
 
 int main() 
 {
-    setlocale(0, "RUS");
+    /*setlocale(0, "RUS");
     Weather weather( "Киров", 49.6601, 58.5966, 5.69, "дождь", 4.27, 100 );
     weather.printWeatherDescription();
-    return 0;
+    return 0;*/
+
+    JsonService js;
+    Weather w = js.getWeather("weather.json");
+
 }
